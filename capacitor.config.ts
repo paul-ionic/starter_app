df065bbe1295b3fa10c9e1c8a425d37003eeb588
Paul LaCaille <paul@ionic.io>
2022-02-02 10:41:13 -0600

    change bundle id

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io.ionic.cs.demo.dev',
   appName: 'starter_app',
   webDir: 'www',
   bundledWebRuntime: false
